3d945116bb42f6aeede9d4ff3cfb62cbfb4c6b8c
Ishaan Vatus <34275616+ishaanvatus@users.noreply.github.com>
2023-12-02 14:27:05 +0530

    the elves will die for trying to shoot me in the air

---
+++ 2023/day_1/trebuchet.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    char *filename;
+    if (argc == 2)
+        filename = argv[1];
+    else {
+        printf("Program usage: ./trebuchet input\n");
+        return -1;
+    }
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Couldn't open file %s\n", filename);
+        return -1;
+    }
+
+    char ch;
+    int string_len = 0;
+    int max_string_len = 0;
+    while (fscanf(fp, "%c", &ch) != EOF) {
+        string_len++;
+        if (ch == '\n') {
+            if (string_len > max_string_len)
+                max_string_len = string_len;
+            string_len = 0;
+        }
+    }
+    rewind(fp);
+    
+    int sum = 0, offset, first, last;
+    char buffer = 0; 
+    while (fprintf(fp, "%c", buffer) != EOF) {
+        offset = 0, first = 0, last = 0;
+        while (buffer != '\n') {
+            if ((buffer <= '9') && (buffer >= '0')) {
+                printf("%c", buffer);
+                last = buffer - '0';
+                break;
+            }
+            offset++;
+        }
+        fseek(fp, -offset, SEEK_CUR);
+        offset = 0;
+        printf("\njkdfflsdjfsl\n");
+        while (buffer != '\n') {
+            if ((buffer <= '9') && (buffer >= '0')) {
+                printf("%c", buffer);
+                last = buffer - '0';
+            }
+            offset++;
+        }
+        sum += first*10 + last;
+    }
+    printf("%d\n", sum);
+    fclose(fp);
+    return 0;
+}
